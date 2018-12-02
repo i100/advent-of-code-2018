e0b11ef05c596774d032d7e294dfc2ec7d9efef1
hf2 <hf2@protonmail.com>
2018-12-02 20:36:50 +1300

    Initial commit

---
+++ day1/day1.scala
@@ -0,0 +1,58 @@
+#!/usr/bin/env scala
+ 
+import scala.io.Source
+
+val SUM_ALL_FREQUENCIES = false 
+val TEST_FOR_REPETITION = true
+
+def sumFrequencies(freqs: List[Int]): Int = {
+  sumFrequencies(0, freqs)
+}
+
+def sumFrequencies(currentFreq: Int, freqs: List[Int]): Int = {
+  freqs match {
+    case Nil => currentFreq
+    case nextFreq :: tail => 
+      sumFrequencies(currentFreq + nextFreq, tail)
+  } 
+}
+
+def findFirstRepeatedFrequency(freqs: List[Int]): Int = {
+  findFirstRepeatedFrequency(0, freqs, List(0))
+}
+
+def findFirstRepeatedFrequency(currentFreq: Int, 
+    freqsRemaining: List[Int], freqsSeen: List[Int]): Int = {
+
+  freqsRemaining match {
+    case Nil =>
+      // get the list again and keep iterating
+      findFirstRepeatedFrequency(currentFreq, 
+        frequencyList, freqsSeen)
+    case nextFreq :: tail =>
+    // there must be a cleverer way!
+    val updatedFreq = currentFreq + nextFreq
+
+    if (freqsSeen.contains(updatedFreq)) {
+      updatedFreq
+    } else {
+      findFirstRepeatedFrequency(updatedFreq, tail, 
+        updatedFreq :: freqsSeen)
+    }
+  }
+}
+
+val frequencyList: List[Int] = Source
+  .fromFile(s"./input")
+  .getLines
+  .toList
+  .map { _.toInt }
+
+if (SUM_ALL_FREQUENCIES) { 
+  println(s"New frequency: ${sumFrequencies(frequencyList)}")
+}
+
+if (TEST_FOR_REPETITION) {
+  val freq = findFirstRepeatedFrequency(frequencyList)
+  println(s"First repeated frequency: ${freq}")
+}
