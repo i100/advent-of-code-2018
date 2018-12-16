793ec4a79973f0bd23d148c0e2dee0a558166864
hf2 <hf2@protonmail.com>
2018-12-16 21:44:25 +1300

    -_-

--- day6/day6.scala
+++ day6/day6.scala
@@ -8,12 +8,18 @@ abstract class GridPoint() {
 }
 
 case class Coord(x: Int, y: Int, id: Int) extends GridPoint { }
-case class Location(
+case class ClosestCoords( // part 1
   x: Int, 
   y: Int, 
   manHattanDistance: Int,
-  closestCoords: List[Coord]
+  coords: List[Coord]
 ) extends GridPoint { } 
+case class TotalDistance( // part 2
+  x: Int, 
+  y: Int, 
+  distance: Int
+)
+val LOCALITY = 10000
 
 val inputCoords = Source.fromFile("./input")
   .getLines
@@ -38,28 +44,22 @@ object Grid {
   val xRange: List[Int] = List.range(minX, maxX + 1) 
   val yRange: List[Int] = List.range(minY, maxY + 1) 
   val coords = inputCoords
+  val rawPoints: List[(Int, Int)] = { for (x <- xRange; y <- yRange) yield (x, y) }
   val gridSize = xRange.length * yRange.length
 
-  lazy val locations: List[Location] = {
-    // the names are very confusing :(
-    def xyIsCoord(x:Int, y: Int): Boolean = {
-      (coords.filter { c => c.x == x && c.y == y }.length >= 1)
-    }
+  def manhattanDistance(p1: (Int, Int), p2: (Int, Int)) = {
+    abs(p1._1 - p2._1) + abs(p1._2 - p2._2)
+  }
+  
+  def distances(x: Int, y: Int): List[(Coord, Int)] = coords.map { c => 
+    (c, manhattanDistance((c.x, c.y), (x, y)))
+  }
 
-    val rawPoints: List[(Int, Int)] = {
-      for (x <- xRange; y <- yRange) yield (x, y) // if ! xyIsCoord(x, y)) yield (x, y)
-    }
-
+  lazy val coordsClosestToPoints: List[ClosestCoords] = {
     def closestCoords(x: Int, y: Int): (Int, List[Coord]) = {
-      def manhattanDistance(p1: (Int, Int), p2: (Int, Int)) = {
-        abs(p1._1 - p2._1) + abs(p1._2 - p2._2)
-      }
-      // ouch
-      val distances: List[(Coord, Int)] = coords.map { c => 
-        (c, manhattanDistance((c.x, c.y), (x, y)))
-      }
-      val minDistance: Int = distances.minBy(_._2)._2
-      val closestCoords: List[Coord] = distances
+      val distancesToPoint = distances(x, y)
+      val minDistance: Int = distancesToPoint.minBy(_._2)._2
+      val closestCoords: List[Coord] = distancesToPoint
         .filter { d => d._2 == minDistance }
         .map { d => d._1 }
 
@@ -68,33 +68,43 @@ object Grid {
 
     rawPoints.map { p => 
       val (distance, coords) = closestCoords(p._1, p._2)
-      Location(p._1, p._2, distance, coords)
+      ClosestCoords(p._1, p._2, distance, coords)
     }
   }
 
-  def getLocation(x: Int, y: Int) = { locations.filter { p => p.x == x && p.y == y }(0) }
+  def getClosestCoords(x: Int, y: Int) = { coordsClosestToPoints.filter { p => p.x == x && p.y == y }(0) }
   def getCoord(x: Int, y: Int) = { coords.filter { p => p.x == x && p.y == y }(0) }
 
-  lazy val edgeLocations: List[Location] = {
+  lazy val edgeClosestCoords: List[ClosestCoords] = {
     val xEdges = xRange.zip(List.fill(xRange.length) { minY }) ::: xRange.zip(List.fill(xRange.length) { maxY })
     val yEdges = List.fill(yRange.length) { minX }.zip(yRange) ::: List.fill(yRange.length) { maxX }.zip(yRange)
     val edges = xEdges ::: yEdges
-    edges.map { p => getLocation(p._1, p._2) }
+    edges.map { p => getClosestCoords(p._1, p._2) }
   }
 
-  lazy val unboundedCoords: List[Coord] = edgeLocations
-    .filter { p => p.closestCoords.length == 1 }
-    .map { p => p.closestCoords(0) }
+  lazy val unboundedCoords: List[Coord] = edgeClosestCoords
+    .filter { p => p.coords.length == 1 }
+    .map { p => p.coords(0) }
     .foldLeft(List[Coord]()) { (acc, p) =>
       if (! acc.contains(p)) p :: acc else acc
     }
+  
+  lazy val totalDistancesToAllCoords: List[TotalDistance] = {
+    rawPoints.map { p =>
+      TotalDistance(
+        p._1, p._2, 
+        distances(p._1, p._2).map { d => d._2 }.sum
+      )
+    }
+  }
 }
 
+// part 1
 def findBoundedAreaSizes() = {
-  Grid.locations.filter { p => 
-    ! (p.closestCoords.length > 1 || Grid.unboundedCoords.contains(p.closestCoords(0)))
+  Grid.coordsClosestToPoints.filter { p => 
+    ! (p.coords.length > 1 || Grid.unboundedCoords.contains(p.coords(0)))
   } 
-    .groupBy(_.closestCoords(0).id)
+    .groupBy(_.coords(0).id)
     .mapValues { l => l.length }
 }
 
@@ -102,13 +112,19 @@ def findLargestBoundedArea() = {
   findBoundedAreaSizes().maxBy(_._2)
 }
 
+// part 2
+def findSizeOfAreaWithLocalCoords() = {
+  Grid.totalDistancesToAllCoords.filter { d => d.distance < LOCALITY }.length
+}
+
+
 val CALC_PART_1 = true
 val CALC_PART_2 = true
 
 if (CALC_PART_1) {
-  println(s"Largest bounded area is (id,count): ${findLargestBoundedArea}")
+  println(s"Largest bounded area is (id, count): ${findLargestBoundedArea}")
 }
 
 if (CALC_PART_2) {
-
+  println(s"Size of region with all local coords: ${findSizeOfAreaWithLocalCoords}")
 }
